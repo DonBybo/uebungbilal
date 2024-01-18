9218e19bbc4d39a501e9e98fc4982c3c4adde273
DonBybo <123815356+DonBybo@users.noreply.github.com>
2024-01-18 15:07:27 +0100

    Add files via upload

---
+++ uebungen-master/uebungen-master/js/app.js
@@ -0,0 +1,26 @@
+
+// This function initializes user media
+function getUserMedia(options, successCallback, failureCallback) {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    return navigator.mediaDevices.getUserMedia(options)
+      .then(successCallback)
+      .catch(failureCallback);
+  }
+  throw new Error('User Media API not supported.');
+}
+
+
+
+
+
+
+
+
+// offline storage
+
+
+}
+
+
+
+
